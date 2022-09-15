ca0a6411926020815cc3064955f7b0e6ce5c7f20
AleJs <alejscript@gmail.com>
2022-09-15 19:25:37 -0300

    bug rutas, registro fer, consumo api

--- services/usuario.js
+++ services/usuario.js
@@ -1,23 +1,37 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = ''
+const baseUrl = "";
+const headers = {
+  headers: {
 
-export const login = async credentials =>{
-    
-    // const {data} = await axios.post(baseUrl, credentials)
-    return true
-}
+    "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    "Access-Control-Allow-Origin": "*",
+  }
+};
+export const login = async (credentials) => {
+  // const {data} = await axios.post(baseUrl, credentials)
+  return true;
+};
 
-export const formRegister = async credentials =>{
-  console.log(credentials)
-    await axios.post(' https://m9y19x66c7.execute-api.us-east-1.amazonaws.com/dev/auth/register', credentials)
+export const formRegister = async (credentials) => {
+  console.log(credentials);
+  await axios
+    .post(
+      "https://m9y19x66c7.execute-api.us-east-1.amazonaws.com/dev/auth/register",
+      credentials,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+        "Access-Control-Allow-Origin": "*",
+      }
+    }    
+    )
     .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-} 
-// export const register = async credentials => {
-
-// }
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
